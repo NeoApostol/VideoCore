586e3b2956783378c7e0daf1fefdf4df16d87736
Alexander Mazurok <mazurok.a.wezom@gmail.com>
2016-11-28 17:52:57 +0200

    Change h264 encoder profile

--- videocore/transforms/Apple/H264Encode.mm
+++ videocore/transforms/Apple/H264Encode.mm
@@ -249,8 +249,7 @@ namespace videocore { namespace Apple {
         }
         
         if(err == noErr) {
-            CFStringRef profileLevel = useBaseline ? kVTProfileLevel_H264_Baseline_AutoLevel : kVTProfileLevel_H264_Main_AutoLevel;
-            
+            CFStringRef profileLevel = kVTProfileLevel_H264_Baseline_3_1; //useBaseline ? kVTProfileLevel_H264_Baseline_AutoLevel : kVTProfileLevel_H264_Main_AutoLevel;
             err = VTSessionSetProperty(session, kVTCompressionPropertyKey_ProfileLevel, profileLevel);
         }
         if(!useBaseline) {
